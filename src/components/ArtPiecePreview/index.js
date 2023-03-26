fecf729ae54dcdd5d640e882596975644cf1ca71
Stephan Niendorf <stephan@stephanmitph.com>
2023-03-26 18:28:26 +0200

    add line breaks

--- src/components/ArtPiecePreview/index.js
+++ src/components/ArtPiecePreview/index.js
@@ -4,8 +4,8 @@ export default function ArtPiecePreview({ imageSource, name, artist }) {
   return (
     <div>
       <Image src={imageSource} alt={name} width={350} height={350} />
-      <p>Artist: {artist}</p>
-      <p>Name: {name}</p>
+      <p><strong>Artist:</strong><br></br> {artist}</p>
+      <p><strong>Name:</strong><br></br> {name}</p>
     </div>
   );
 }
